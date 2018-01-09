42a0a72db8ca05f1ec845983950a254db2e8c787
alatel01 <alatel01@dcs.bbk.ac.uk>
2018-01-09 14:44:34 +0000

    first commit

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alatel01 on 09/01/2018.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World! " + Example.aMethod(1));
+
+    }
+    public static int aMethod (int i){
+        return 0;
+    }
+}
---
+++ test/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alatel01 on 09/01/2018.
+ */
+public class ExampleTest {
+    @Test
+    public void aMethod() throws Exception {
+        assert (Example.aMethod(1) == 0);
+    }
+
+}
